b264e390efcd1e8cc352276d30606c62854d073e
Raymond <rtduong@uw.edu>
2016-03-31 23:12:46 -0700

    Initialize project

---
+++ Kaleido/app/src/main/java/edu/uw/rtduong/kaleido/MenuActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.rtduong.kaleido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
