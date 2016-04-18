108500a50394aaad2ddc6a97bb69487f5669779d
Raymond <rtduong@uw.edu>
2016-04-18 13:32:09 -0700

    Add Settings activity and layout

--- Kaleido/app/src/main/java/edu/uw/rtduong/kaleido/MenuActivity.java
+++ Kaleido/app/src/main/java/edu/uw/rtduong/kaleido/MenuActivity.java
@@ -2,6 +2,8 @@ package edu.uw.rtduong.kaleido;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        final Button penButton = (Button) this.findViewById(R.id.btnSettingsId);
+        penButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
