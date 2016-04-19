ae20945aa35eff3a9215212f8594d9882377afaf
Raymond <rtduong@uw.edu>
2016-04-18 21:58:58 -0700

    Create Select Story activity

---
+++ Kaleido/app/src/main/java/edu/uw/rtduong/kaleido/SelectStoryActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.rtduong.kaleido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectStoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_story);
+    }
+}
